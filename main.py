32f91c678c72806a126e0d2a793ccf426af7ec1c
LucasRibeiroRJBR <lucasribeirorjbr@gmail.com>
2021-04-03 22:04:45 -0300

    Modifications on graphics and main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from PyQt5 import uic, QtWidget, QtGui
+from PyQt5 import uic, QtWidgets, QtGui
+from bs4 import BeautifulSoup
+import requests
+
 
 def foo():
-    pass
+    url = tela.lineEdit.text()
+    print(url)
+    sopa = BeautifulSoup(requests.get(url).text.encode('utf8'), 'html.parser')
 
-app = QtWidget.QApplication([])
+
+
+
+app = QtWidgets.QApplication([])
 
 tela = uic.loadUi('UI/main_window.ui')
+
+tela.pushButton.clicked.connect(foo)
 
 tela.show()
 app.exec_()
