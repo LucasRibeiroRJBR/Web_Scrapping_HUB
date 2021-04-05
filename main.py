fc95a8b9aebf2fe54e48cf01493b5910443070ac
LucasRibeiroRJBR <lucasribeirorjbr@gmail.com>
2021-04-05 19:55:20 -0300

    Listing tags

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from PyQt5 import uic, QtWidgets, QtGui
 from bs4 import BeautifulSoup
-import requests
+import requests, lxml
 
 
 def foo():
+    tela.cb_tag.clear()
+
     url = tela.lineEdit.text()
-    print(url)
-    sopa = BeautifulSoup(requests.get(url).text.encode('utf8'), 'html.parser')
+    sopa = BeautifulSoup(requests.get(url).text.encode('utf8'), 'lxml')
 
+    tags = []
 
+    for i in sopa.find_all(True):
+        tags.append(i.name)
+
+    tag_names = set(tags)
+
+    for tag in tag_names:
+        tela.cb_tag.addItem(tag)
 
 
 app = QtWidgets.QApplication([])
@@ -18,4 +27,4 @@ tela = uic.loadUi('UI/main_window.ui')
 tela.pushButton.clicked.connect(foo)
 
 tela.show()
-app.exec_()
+app.exec()
